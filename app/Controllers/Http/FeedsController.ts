68ceebc132ee7517ecb05c32a65fe85dd8b6a522
Toni Moreira <toni@itentecnologia.com.br>
2022-05-04 15:34:10 -0300

    Update algoritm

--- app/Controllers/Http/FeedsController.ts
+++ app/Controllers/Http/FeedsController.ts
@@ -5,6 +5,7 @@ import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import UserMatch from "App/Models/UserMatch"
 import UserFeedView from "App/Models/UserFeedView"
 import Database from "@ioc:Adonis/Lucid/Database"
+import User from "App/Models/User"
 
 export default class FeedsController {
 
@@ -18,13 +19,59 @@ export default class FeedsController {
 
     async randomUser({auth, response}) {
         const user = auth.user
-        const calc = `ABS(((sun - ${user.sun}) + (moon - ${user.moon}) + (ascendant - ${user.ascendant}))/10.8)`
-        const users = await Database.rawQuery(`SELECT *, ${calc} as compatibility FROM users WHERE id != ${user.id} AND id NOT IN (SELECT target_id FROM user_feed_views WHERE user_id = ${user.id}) ORDER BY ${calc} DESC;`)
+        const users = await Database.rawQuery(`SELECT (sun + moon + ascendant)/3 as compatibility, user_id FROM (SELECT
+            CASE WHEN sun < 1 THEN 100
+                WHEN sun < 2 THEN 70
+                WHEN sun < 3 THEN 100
+                WHEN sun < 4 THEN 70
+                WHEN sun < 5 THEN 100
+                WHEN sun < 6 THEN 40
+                WHEN sun < 7 THEN 80
+                WHEN sun < 8 THEN 40
+                WHEN sun < 9 THEN 100
+                WHEN sun < 10 THEN 70
+                WHEN sun < 11 THEN 100
+                WHEN sun < 12 THEN 70
+            END as sun,
+            CASE WHEN moon < 1 THEN 100
+                WHEN moon < 2 THEN 70
+                WHEN moon < 3 THEN 100
+                WHEN moon < 4 THEN 70
+                WHEN moon < 5 THEN 100
+                WHEN moon < 6 THEN 40
+                WHEN moon < 7 THEN 80
+                WHEN moon < 8 THEN 40
+                WHEN moon < 9 THEN 100
+                WHEN moon < 10 THEN 70
+                WHEN moon < 11 THEN 100
+                WHEN moon < 12 THEN 70
+            END as moon,
+            CASE WHEN ascendant < 1 THEN 100
+                WHEN ascendant < 2 THEN 70
+                WHEN ascendant < 3 THEN 100
+                WHEN ascendant < 4 THEN 70
+                WHEN ascendant < 5 THEN 100
+                WHEN ascendant < 6 THEN 40
+                WHEN ascendant < 7 THEN 80
+                WHEN ascendant < 8 THEN 40
+                WHEN ascendant < 9 THEN 100
+                WHEN ascendant < 10 THEN 70
+                WHEN ascendant < 11 THEN 100
+                WHEN ascendant < 12 THEN 70
+            END as ascendant,
+            user_id
+            FROM (SELECT
+                ABS(${user.sun/30} - sun/30) as sun,
+                ABS(${user.moon/30} - moon/30) as moon,
+                ABS(${user.ascendant/30} - ascendant/30) as ascendant,
+                id as user_id
+            FROM users WHERE id != ${user.id} AND id NOT IN (SELECT target_id FROM user_feed_views WHERE user_id = ${user.id})) as user_compatibility) as data ORDER BY compatibility LIMIT 1`)
 
         if (!users.rows.length) {
             return response.noContent()
         }
-        return users.rows[0]
+        const randomUser = await User.findOrFail(users.rows[0].user_id)
+        return {...randomUser.serialize(), compatibility: users.rows[0].compatibility}
     }
 
     async avaliate({request, auth, response}) {
